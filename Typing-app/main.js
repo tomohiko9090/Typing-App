40cd3b3625ef525705f38c103d3270c1d3ab68cd
hikotomo <tomo.k9090@gmail.com>
2022-09-26 02:39:41 +0900

    files modify

--- Typing-app/main.js
+++ Typing-app/main.js
@@ -1,41 +1,56 @@
-import StopWatch from './StopWatch.js'
-
 let width
 let color
 
 const app = Vue.createApp({
 
-  components: {
-    StopWatch
-  },
-
+  // データプロパティ
   data(){
     return {
+      active : false, // 実行状態
+      start : 0, // startを押した時刻
+      timer : 0, // setInterval()の格納用
+      interval : 0, // 計測時間
+      accum : 0, // 累積時間(stopしたとき用)
+
       startFlg: "",
       current_question: "",
+      // questions:[
+      //   "Motivation Cloud series", //  
+      //   "Labo Communication Cloud",
+      //   "i-Company CLUB",
+      //   "Motivation Index score",
+      //   "Customer Reliability Engineering",
+      //   "Process Design",
+      //   "Product Management",
+      //   "Site Reliability Engineering",
+      //   "Weekly Active Users",
+      //   "Team Engagement Plan",
+      // ],
       questions:[
-        "Motivation Cloud series",
-        "Labo Communication Cloud",
-        "i-Company CLUB",
-        "Motivation Index score",
-        "Customer Reliability Engineering",
-        "Process Design",
-        "Product Management",
-        "Site Reliability Engineering",
-        "Weekly Active Users",
-        "Team Engagement Plan",
+        "1", //  
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
       ],
       typeBox: "",
       current_question_counts: 0,
-      question_counts: 0
+      question_counts: 0,
     }
   },
 
+  // 算出プロパティ
   computed:{
     styleObject:function(){
       width = 10 * this.current_question_counts + "%"
       if(this.current_question_counts == 10){
         color = "#03a9f4"
+        this.stopTimer()
       }else{
         color = "orange"
       }
@@ -46,8 +61,19 @@ const app = Vue.createApp({
     }
   },
 
+  // メソッド
   methods:{
-    gameStart:function(){
+    startTimer(){
+        this.active = true;
+        this.start = Date.now();
+        this.timer = setInterval(()=>{ this.interval = this.accum + (Date.now() - this.start) * 0.001;}, 10); // 10msごとに現在時刻とstartを押した時刻の差を足す
+    },
+    stopTimer(){
+        this.active = false;
+        this.accum = this.interval;
+        clearInterval(this.timer);
+    },
+    gameStart(){
       this.startFlg = true;
       // カーソルをinputにフォーカスする
       this.$nextTick(function(){
@@ -56,20 +82,20 @@ const app = Vue.createApp({
     }
   },
 
-  // 描画されたタイミングで実行される
+  // ライフサイクルフックの一つ | 描画されたタイミングで実行される -> つまり1回
   mounted:function(){
     this.current_question = this.questions[0]
     this.question_counts = this.questions.length
   },
 
-  // データが切り替わる度に動作する
+  // ウォッチャ | データが切り替わる度に動作する -> 監視
   watch:{
     typeBox:function(e){
       if(e == this.current_question){
-        this.questions.splice(0, 1) //配列一つ目を消去
-        this.current_question = this.questions[0]
-        this.typeBox = ""
-        this.current_question_counts = this.current_question_counts + 1 // 回答した数が増える
+        this.questions.splice(0, 1) //1.配列一つ目を消去
+        this.current_question = this.questions[0] //2.一番先頭のお題を格納
+        this.typeBox = ""  //3.テキストボックスを空の状態に変更
+        this.current_question_counts = this.current_question_counts + 1 // 4.回答した数が増やす
       }
     }
   }
