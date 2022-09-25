7ae1af3fcd1fc9ea50251383b443a1da8ce7adf7
hikotomo <tomo.k9090@gmail.com>
2022-09-26 08:13:04 +0900

    files modify

---
+++ test/main.js
@@ -0,0 +1,27 @@
+// 実験1
+const app1 = Vue.createApp({
+    data() {
+    return {
+      myName: '葛葉1'
+    }
+  }
+}).mount("#app1")
+
+// 実験2（入力された値が引数になる）
+const app2 = Vue.createApp({
+  methods:{
+    inputEvent(event) {
+      console.log(event.target.value);
+    }
+  }
+}).mount("#app2")
+
+// 実験3
+const app3 = Vue.createApp({
+    data() {
+    return {
+      myName: '葛葉3'
+    }
+  }
+}).mount("#app3")
+
